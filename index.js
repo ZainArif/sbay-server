638d256290234925bc163bb84f6213d0c1b45a14
Muddabirchohan <muddabir22@gmail.com>
2019-06-18 15:12:42 +0500

    added

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.use("/products",productRoutes);
 //     'preflightContinue': false
 //   }));
 
-// app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
